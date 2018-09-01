f101285dc4e1769cc93e45829b5baf1ea9b4c096
Abhishek Yadav <abhishek.yadav.cs@gmail.com>
2018-08-31 17:04:25 -0700

    first commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Abhishek Yadav on 8/31/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
