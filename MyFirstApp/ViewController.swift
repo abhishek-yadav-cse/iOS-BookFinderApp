6cbacaec3da71fa8af424fccdd3deec6bea317ff
Abhishek Yadav <abhishek.yadav.cs@gmail.com>
2018-09-04 22:37:27 -0700

    label

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var bookLabelName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
